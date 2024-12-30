b37990886a5ae48f16f61752a9629fb1c42cdc55
michael isbell <prenezisbell@gmail.com>
2024-12-30 09:02:06 -0500

    comment test

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import styles from './styles';
+//comment
 
 const Stack = createStackNavigator();
 
